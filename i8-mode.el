8480013ca6a3335fb7631aef8c0667f43b6eb76a
Gary Benson <gbenson@redhat.com>
2015-11-12 13:07:08 +0000

    Fixup

--- i8-mode.el
+++ i8-mode.el
@@ -1,4 +1,3 @@
-(add-to-list 'load-path "~/work/i8c/scratch/contrib")
-
-(autoload 'i8-mode "i8" "XXX documentation." t)
+(add-to-list 'load-path "~/work/i8c/mode/contrib")
+(autoload 'i8-mode "i8-mode" "XXX documentation." t)
 (add-to-list 'auto-mode-alist '("\\.i8\\'" . i8-mode))
