7aac38381f5508eea18dec1839dc7b6ee12cd5f8
Gary Benson <gbenson@redhat.com>
2019-06-26 10:06:58 +0100

    Use i8-mode from scratch

--- i8-mode.el
+++ i8-mode.el
@@ -1,3 +1,3 @@
-(add-to-list 'load-path "~/work/i8c/mode/contrib")
+(add-to-list 'load-path "~/work/i8c/scratch/contrib")
 (autoload 'i8-mode "i8-mode" "XXX documentation." t)
 (add-to-list 'auto-mode-alist '("\\.i8\\'" . i8-mode))
